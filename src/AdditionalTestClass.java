fef41d5d86c239fbb0a1e5559cb3dd5d7933671f
hussainmohsin042 <hussainmohsin042@gmail.com>
2022-03-09 16:07:47 +0530

    This is commit from Mohsin Hussain Asian Time Zone

---
+++ src/AdditionalTestClass.java
@@ -0,0 +1,12 @@
+
+public class AdditionalTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is an addition class added By Mohsin Hussain");
+		System.out.println("Test1234567890");
+
+	}
+
+}
